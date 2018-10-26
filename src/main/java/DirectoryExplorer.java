be46ca3523bc826608a5771a90fe7db7bfab62be
d.horishenko <d.horischenko@pfsoft.org>
2018-10-26 11:11:22 +0300

    2

--- src/main/java/DirectoryExplorer.java
+++ src/main/java/DirectoryExplorer.java
@@ -1,4 +1,6 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 
 
 public class DirectoryExplorer {
@@ -17,9 +19,28 @@ public class DirectoryExplorer {
             for (int i = 0; i < files.length; i++) {
                 if (!file.getName().equals(files[i].getName())) {
                     if (filesCompare.isFilesEquals(file, files[i])) {
-                        file.renameTo(new File(files[i].getAbsolutePath() + "_duplicated"));
+                        String newFileName = files[i].getAbsolutePath() + "_duplicated";
+                        File renamedFile = new File(newFileName);
+                        file.renameTo(renamedFile);
+                        logAllDuplicates("File: " + file.getName() + " was renamed to: " + renamedFile.getName() + "\n");
                     }
                 }
+            }
+        }
+    }
+
+    private void logAllDuplicates(String textToWrite){
+        BufferedWriter writer = null;
+        try {
+            File log = new File("duplicates_log.txt");
+            writer = new BufferedWriter(new FileWriter(log, true));
+            writer.write(textToWrite);
+        }catch (Exception e){
+            e.getMessage();
+        }finally {
+            try{
+                writer.close();
+            }catch (Exception e){
             }
         }
     }
